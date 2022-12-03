121b1e8c32cef4691a56abf33ef21b98b93b06ef
Agatha20808077 <96261296+Agatha20808077@users.noreply.github.com>
2022-12-03 11:54:27 -0300

    Add files via upload

--- js/Player.js
+++ js/Player.js
@@ -4,10 +4,7 @@ class Player {
         this.index = null;
         this.positionX = 0;
         this.positionY = 0;
-        this.fuel = 185;
-        this.life = 185;
-        this.rank = 0;
-        this.score = 0;
+
     }
     //pega a contagem dos players no banco de dados
     getCount(){
@@ -36,10 +33,6 @@ class Player {
             name: this.name,
             positionX: this.positionX,
             positionY: this.positionY,
-            fuel: this.fuel,
-            life: this.life,
-            rank: this.rank,
-            score: this.score,
         });
     }
 
@@ -57,25 +50,7 @@ class Player {
     database.ref(playerIndex).update({
         positionX: this.positionX,
         positionY: this.positionY,
-        fuel: this.fuel,
-        life: this.life,
-        rank: this.rank,
-        score: this.score,
     });
-    }
-
-    //pega o valor do BD
-    getCarsAtEnd(){
-        database.ref('carsAtEnd').on("value", (data)=>{
-            this.rank = data.val();
-        })
-    }
-
-    //atualiza o valor no banco de dados
-    static updateCarsAtEnd(rank){
-        database.ref("/").update({
-            carsAtEnd: rank
-        })
     }
 
 }//classe
--- sketch.js
+++ sketch.js
@@ -5,19 +5,12 @@ var gameState, playerCount;
 var allPlayers, fundoImg;
 var carro1, carro2, carro1Img, carro2Img;
 var carros = [];
-var gCoin, gFuel, gObstacles;
-var coinImg, fuelImg, obstacle1Image, obstacle2Image, lifeImg;
 
 function preload(){
-    bgImg = loadImage("./imagens/planodefundo.png");
-    fundoImg = loadImage("./imagens/track.jpg");
-    carro1Img = loadImage("./imagens/car1.png");
-    carro2Img = loadImage("./imagens/car2.png");
-    coinImg = loadImage("./imagens/goldCoin.png");
-    fuelImg = loadImage("./imagens/fuel.png");
-    obstacle1Image = loadImage("./imagens/obstacle1.png");
-    obstacle2Image = loadImage("./imagens/obstacle2.png");
-    lifeImg = loadImage("./imagens/life.png");
+    bgImg = loadImage("./assets/planodefundo.png");
+    fundoImg = loadImage("./assets/track.jpg");
+    carro1Img = loadImage("./assets/car1.png");
+    carro2Img = loadImage("./assets/car2.png");
 }
 
 function setup(){
